f7b168017c605659b874832cabdb94a3ca7ee7ec
Sudheer Kumar <sudheermsdvk@gmail.com>
2024-07-16 21:16:44 +0530

    Delete my_library directory

--- my_library/README.md
+++
@@ -1,6 +0,0 @@
-#My Library
-A pYTHON LIBRARY for loan prediction and more.
-##Installation
-You can install the library using pip:
-```sh
-pip install my_library
--- my_library/my_library/module.py
+++
@@ -1,8 +0,0 @@
-def say_hello(name):
-    return f"Hello, {name}!"
-class LoanPredictor:
-    def __init__(self,model):
-        self.model=model
-    def predict(self,data):
-        return self.model.predict(data)
-        
